215bc6cd29044ffeefe66dd572167bc64dec77d4
Chelsea Policarpio <chelsea@Chelseas-MacBook-Air.local>
2026-08-30 19:33:23 +0800

    Allow Supabase dress images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "placehold.co"],
+    domains: ["images.unsplash.com", "placehold.co", "lkrfcxlrnlivtugshqeo.supabase.co"],
   },
 };
 
